9492685ea85110aae91c18a45c4f48ea4d6cb96d
Ravi Kumar Gupta <babaravibbb@gmail.com>
2024-02-26 23:42:18 +0530

    Spring AI Image Generator

---
+++ src/test/java/com/example/springaiimagegenerator/SpringAiImageGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springaiimagegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiImageGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
